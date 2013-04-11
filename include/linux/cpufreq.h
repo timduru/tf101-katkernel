285566bb4458a51a9424a91e65a05c73a5ee4765
timduru <timduru@gmail.com>
2013-04-11 10:37:13 +0200

    Add BrazilianWax cpufreq governor +fix

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -364,7 +364,11 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTASS2)
 extern struct cpufreq_governor cpufreq_gov_smartass2;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_interactive)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_BRAZILIANWAX)
+extern struct cpufreq_governor cpufreq_gov_brazilianwax;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_brazilianwax)
 #endif
+
 
 
 /*********************************************************************
