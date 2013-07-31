011623ed4d58344878095ff6fc47d737fe823bab
timduru <timduru@gmail.com>
2013-07-31 15:34:56 +0200

    Use MMC full block size

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -2304,11 +2304,13 @@ int sdhci_add_host(struct sdhci_host *host)
 	} else {
 		mmc->max_blk_size = (caps[0] & SDHCI_MAX_BLOCK_MASK) >>
 				SDHCI_MAX_BLOCK_SHIFT;
+	#ifndef CONFIG_MMC_SDHCI_NATIVE_BLOCKSIZE
 		if (mmc->max_blk_size >= 3) {
 			printk(KERN_WARNING "%s: Invalid maximum block size, "
 				"assuming 512 bytes\n", mmc_hostname(mmc));
 			mmc->max_blk_size = 0;
 		}
+	#endif
 	}
 
 	mmc->max_blk_size = 512 << mmc->max_blk_size;
@@ -2317,6 +2319,12 @@ int sdhci_add_host(struct sdhci_host *host)
 	 * Maximum block count.
 	 */
 	mmc->max_blk_count = (host->quirks & SDHCI_QUIRK_NO_MULTIBLOCK) ? 1 : 65535;
+
+	#ifdef CONFIG_MMC_SDHCI_NATIVE_BLOCKSIZE  	
+  	  printk(KERN_INFO "%s: mss %u mrs %u mbs %u mbc %u\n", mmc_hostname(mmc),
+    	  mmc->max_seg_size, mmc->max_req_size, mmc->max_blk_size,
+    	  mmc->max_blk_count);	
+	#endif
 
 	/*
 	 * Init tasklets.
